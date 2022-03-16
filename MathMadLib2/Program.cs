4b3225fc827a135d38859e1305e6e7e1388ac733
sheturah <57009932+sheturah@users.noreply.github.com>
2022-03-16 17:41:39 -0400

    first commit

---
+++ MathMadLib2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1321DailyCodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y, z, w;
+            float answer;
+
+            Console.WriteLine("[Math Mad Libs]");
+
+            Console.WriteLine("\nx + (y / z) * w = ? ");
+            Console.WriteLine("Enter the first term: ");
+            x = Single.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("\n" + x + " + (y / z) * w = ? ");
+            Console.WriteLine("Enter the second term: ");
+            y = Single.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n" + x + " + (" + y + " / z) * w = ? ");
+            Console.WriteLine("Enter the third term: ");
+            z = Single.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n" + x + " + (" + y + " / " + z + ") * w = ? ");
+            Console.WriteLine("Enter the first term: ");
+            w = Single.Parse(Console.ReadLine());
+
+
+            answer = x + (y / z) * w;
+            Console.WriteLine("\n" + x + " + (" + y + " / " + z + ") * " + w + " = " + answer);
+        }
+    }
+}
